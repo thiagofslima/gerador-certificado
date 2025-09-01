eeef50f6796fb6186dc8f901586fd7051aa4a919
Thiago Lima <thiagofslima@gmail.com>
2025-09-01 16:27:10 +0200

    Chama lista de certificados para visualização

--- src/app/pages/certificados/certificados.component.ts
+++ src/app/pages/certificados/certificados.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
+import { ItemCertificadoComponent } from "../../_components/item-certificado/item-certificado.component";
 
 @Component({
   selector: 'app-certificados',
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, ItemCertificadoComponent],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.css'
 })
