f023547c8afca359982a41ed41f582a06dbccbe5
Thiago Lima <thiagofslima@gmail.com>
2025-09-19 13:04:12 +0200

    criando rotas com RouterLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CertificadoComponent } from './pages/certificado/certificado.component';
+import { CertificadosComponent } from './pages/certificados/certificados.component';
+import { CertificadoFormComponent } from './pages/certificado-form/certificado-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: CertificadosComponent
+  },
+  {
+    path: "certificados/novo",
+    component: CertificadoFormComponent
+  },
+  {
+    path: "certificados/:id",
+    component: CertificadoComponent
+  },
+];
--- src/app/pages/certificado/certificado.component.ts
+++ src/app/pages/certificado/certificado.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certificado',
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, RouterLink],
   templateUrl: './certificado.component.html',
   styleUrl: './certificado.component.css'
 })
