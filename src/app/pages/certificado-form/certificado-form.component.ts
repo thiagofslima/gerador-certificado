304b6fc4e15f45f13d74daf1ae73fe35d8e21e4e
Thiago Lima <thiagofslima@gmail.com>
2025-09-20 10:34:42 +0200

    add ngModel no form

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from "../../_components/primary-button/primary-button.component";
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FormsModule],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.css'
 })
 export class CertificadoFormComponent {
-
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['Angular', 'React'];
 }
