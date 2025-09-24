20a0ff15f9d8cdf5322b8f974caf6d207ff2bbe8
Thiago Lima <thiagofslima@gmail.com>
2025-09-24 11:22:25 +0200

    Add Input nos botões

--- src/app/pages/certificados/certificados.component.ts
+++ src/app/pages/certificados/certificados.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
 import { ItemCertificadoComponent } from "../../_components/item-certificado/item-certificado.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-certificados',
-  imports: [SecondaryButtonComponent, ItemCertificadoComponent],
+  imports: [SecondaryButtonComponent, ItemCertificadoComponent, RouterLink],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.css'
 })
