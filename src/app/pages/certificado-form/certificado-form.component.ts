c8e781f742424a543493c7eb5e84124942be7b52
Thiago Lima <thiagofslima@gmail.com>
2025-09-24 12:20:55 +0200

    add dataEmissao na interface

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -14,7 +14,8 @@ import { Certificado } from '../../interfaces/certificado';
 export class CertificadoFormComponent {
   certificado: Certificado = {
     atividades: [],
-    nome: ''
+    nome: '',
+    dataEmissao: ''
   };
   atividade: string = '';
 
@@ -39,6 +40,18 @@ export class CertificadoFormComponent {
     if (!this.formValido())
       return;
 
-    console.log(this.certificado);
+    this.certificado.dataEmissao = this.dataAtual();
+
+    console.log(this.certificado)
+  }
+
+  dataAtual() {
+    const dataAtual = new Date();
+    const dia = String(dataAtual.getDate()).padStart(2, '0');
+    const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
+    const ano = dataAtual.getFullYear();
+
+    const dataFormatada = `${dia}/${mes}/${ano}`;
+    return dataFormatada;
   }
 }
