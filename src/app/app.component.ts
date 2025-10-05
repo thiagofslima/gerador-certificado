9879ba93d1c9057f2cf900f7b09645862b289d1b
Thiago Lima <thiagofslima@gmail.com>
2025-10-05 22:11:56 +0200

    remover imports não utilizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
-import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
-import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
-import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
+
+
+
 import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
-import { CertificadosComponent } from "./pages/certificados/certificados.component";
-import { CertificadoFormComponent } from "./pages/certificado-form/certificado-form.component";
-import { CertificadoComponent } from "./pages/certificado/certificado.component";
+
+
+
 import { CertificadoService } from './_services/certificado.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent, BaseUiComponent, CertificadosComponent, CertificadoFormComponent, CertificadoComponent],
+  imports: [RouterOutlet, NavbarComponent, BaseUiComponent,  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
